313401856d95cfce68429648ddb10282d400aeed
FleurNk <91100184+FleurNk@users.noreply.github.com>
2021-12-17 13:58:01 -0600

    Delete ejercicio2-bucles.py

--- ejercicio2-bucles.py
+++
@@ -1,9 +0,0 @@
-#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
-
-x = 1
-print("¿Cual es tu edad?")
-edad = int(input())
-while (x<edad):
-    print(x)
-    x = x+1
-print(x)
